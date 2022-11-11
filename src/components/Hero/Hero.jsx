7d49ac1deffbd1af06f8b3534737547d1452b616
Andres <andrescn_2010@hotmail.com>
2022-11-11 19:24:48 -0300

    Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,12 +5,12 @@ const Hero = () => {
     return (
         <div className='hero-container'>
             <div className='text-container'>
-                <h1 className='text'>Ten a mano los eventos mas interesantes sobre el mundo IT en un solo lugar!</h1>
+                <h1 className='text'>Ten a mano los <br /> eventos mas <br /> interesantes sobre el mundo IT en un <br />  solo lugar!</h1>
             </div>
             <div className='second-section-container'>
                 <img className='image' src={HeroImage} alt="Hero Image" />
-                <div className='second-text-container'>Lorem ipsun azteca mandangon sobietico</div>
-                <div className='button-container'>Get a Ticket</div>
+                <div className='second-text-container'><h3 className='image-text'>Holasdasdasdasdasd  sa d sa d</h3></div>
+                <div className='button-container'><button className='cta-button'>Get a Ticket</button></div>
             </div>
         </div>
     )
