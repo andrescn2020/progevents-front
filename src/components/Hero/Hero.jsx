9b6db00fa5727a899db23bded4acb0e9387afd1c
Andres <andrescn_2010@hotmail.com>
2022-11-23 20:19:54 -0300

    Phone Responsive Height

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
                 <div className="logo-container">
                     <h2 className="logo"><strong>prog</strong><span style={{ color: "#B9134F" }}>events</span></h2>
                 </div>
-                <h1 className='text'>Ten a mano los <br /> eventos mas <br /> interesantes sobre el <span style={{ color: "#B9134F" }}>mundo IT</span>  en un <br />  solo lugar!</h1>
+                <h1 className='text'>Ten a mano los <br /> eventos y webinars mas <br /> interesantes sobre el <span style={{ color: "#B9134F" }}>mundo IT</span>  en un <br />  solo lugar!</h1>
                 <div className='link-container'>
                     <a id='events-links' className='btn btn-secondary' href='#filters'>Ver Eventos</a>
                 </div>
