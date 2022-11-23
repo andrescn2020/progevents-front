97a0933f2c1d171045ce387b5aa386b36a46fb83
Andres <andrescn_2010@hotmail.com>
2022-11-23 14:52:10 -0300

    Component h3 modificado 2

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -34,7 +34,7 @@ const Contact = () => {
 
     return (
         <div className='formandtitle-container'>
-            <h3 className='form-title'>Para consultas y solicitudes para agregar algun evento nuevo no dudes en contactarme!</h3>
+            <h3 className='form-title'>Para consultas y solicitudes para agregar algún evento nuevo no dudes en contactarme!</h3>
             <form className="background" onSubmit={handleSubmit(sendEmail)}>
                 <div className="container-form">
                     <div className="screen">
